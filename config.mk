098d0b9578ced41316add51158017c0b880614f1
dbro <danbrown@gmail.com>
2010-11-20 08:55:31 -0800

    initial commit of dmenu 4.2.1

---
+++ config.mk
@@ -0,0 +1,27 @@
+# dmenu version
+VERSION = 4.2.1
+
+# Customize below to fit your system
+
+# paths
+PREFIX = /usr/local
+MANPREFIX = ${PREFIX}/share/man
+
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
+
+# Xinerama, comment if you don't want it
+XINERAMALIBS  = -lXinerama
+XINERAMAFLAGS = -DXINERAMA
+
+# includes and libs
+INCS = -I${X11INC}
+LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS}
+
+# flags
+CPPFLAGS = -D_BSD_SOURCE -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
+CFLAGS   = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+LDFLAGS  = -s ${LIBS}
+
+# compiler and linker
+CC = cc
